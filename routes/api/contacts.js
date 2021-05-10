9fd397c5e554ea4f5e4c2f5c96a1da28d277ad91
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2021-05-11 00:10:31 +0300

    adds routers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contacts = require("../../model/index");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  contacts.listContacts().then((data) => res.json(data));
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  contacts.getContactById(req.params.contactId).then((data) => res.json(data));
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  contacts.addContact(req.body).then((data) => res.json(data));
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  contacts.removeContact(req.params.contactId).then((data) => res.json(data));
+});
 
-module.exports = router
+router.patch("/:contactId", async (req, res, next) => {
+  contacts
+    .updateContact(req.params.contactId, req.body)
+    .then((data) => res.json(data));
+});
+
+module.exports = router;
